0711fba898dc210483aaa75b26bb73c5e1d14f95
Binghan Lin <binghan.lin@shoncloud.com>
2021-07-24 12:00:09 +0800

    add incompresible domain

--- src/RFBCMCFD/simulationDomain/include/IncompressibleDomain.hpp
+++ src/RFBCMCFD/simulationDomain/include/IncompressibleDomain.hpp
@@ -1,5 +1,5 @@
-#ifndef SIMULATIONDOMAIN_HPP
-#define SIMULATIONDOMAIN_HPP
+#ifndef INCOMPRESSIBLEDOMAIN_HPP
+#define INCOMPRESSIBLEDOMAIN_HPP
 #include "enumMap.hpp"
 #include "json.h"
 
@@ -17,13 +17,13 @@ class MeshData;
 class ControlData;
 class ScalarConditionPool;
 
-class ScalarTransportDomain
+class IncompressibleDomain
 {
    public:
-    ScalarTransportDomain(ControlData* controlData, MQBasis* RBFBasis,
-                          MeshData* meshData,
-                          ScalarConditionPool* conditionPool);
-    ~ScalarTransportDomain(){};
+    IncompressibleDomain(ControlData* controlData, MQBasis* RBFBasis,
+                         MeshData* meshData,
+                         ScalarConditionPool* conditionPool);
+    ~IncompressibleDomain(){};
 
     void showSummary();
     void solveDomain();
@@ -37,25 +37,29 @@ class ScalarTransportDomain
     ScalarConditionPool* conditionPool_;
 
     // physicsControl
+    double viscosity_;
     double density_;
-    double diffusionCoeff_;
-    std::array<double, 3> convectionVel_;
 
     // solverConstrol
     double neighborRadius_;
     double endTime_;
     double tStepSize_;
     double writeInterval_;
-    double theta_;
-    systemSateType systemSateType_;
+    double crankNicolsonEpsilon_;
+    int crankNicolsonMaxIter_;
     size_t dim_;
     double currentTime_;
 
     // matrix
     Eigen::SparseMatrix<double> varCoeffMatrix_;
-    Eigen::VectorXd varRhs_;
-    Eigen::VectorXd varSol_;
-    Eigen::VectorXd preVarSol_;
+    Eigen::VectorXd allRhs_;
+    Eigen::VectorXd velSol_;
+    Eigen::VectorXd preVelSol_;
+    Eigen::VectorXd pSol_;
+    Eigen::VectorXd prePSol_;
+
+    Eigen::SparseMatrix<double> phiCoeffMatrix_;
+    Eigen::SparseMatrix<double> velCoeffMatrix_;
 
     Eigen::SparseMatrix<double> laplaceMatrix_;
     Eigen::SparseMatrix<double> dxMatrix_;
